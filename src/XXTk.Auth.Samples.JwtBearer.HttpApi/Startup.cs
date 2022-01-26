b7990ae90fed476d7c3049d6e953eea268828cf0
jiajianjun <xiaoxiaotank.j.jia@yzw.cn>
2022-01-26 18:21:02 +0800

    swagger增加authorization头

--- src/XXTk.Auth.Samples.JwtBearer.HttpApi/Startup.cs
+++ src/XXTk.Auth.Samples.JwtBearer.HttpApi/Startup.cs
@@ -128,6 +128,27 @@ namespace XXTk.Auth.Samples.JwtBearer.HttpApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "XXTk.Auth.Samples.Jwt.HttpApi", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+                c.AddSecurityRequirement(securityRequirement);
             });
         }
 
